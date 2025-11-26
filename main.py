98c98f1e19cab6759031417cde4dee746872b1ff
Saskisasff <sankinvadim545@gmail.com>
2025-11-27 00:03:15 +0300

    Добавление режимов

--- main.py
+++ main.py
@@ -33,24 +33,34 @@ a = np.full((9,9), '#')
 bomb = []
 for i in range(randint(4, 6)):
     bomb.append((randint(0,8), randint(0,8)))
-for i in range(9):
-    for j in range(9):
-        if (i, j) in bomb:
-            a[i][j] = '@'
-print(bomb)
+# for i in range(9):
+#     for j in range(9):
+#         if (i, j) in bomb:
+#             a[i][j] = '@'
+# print(bomb)
 print(a)
 while True:
-    coord_x = int(input())
-    coord_y = int(input())
-    if (coord_x, coord_y) in bomb:
-        bomb.pop(bomb.index((coord_x, coord_y)))
-    a[coord_x][coord_y] = '-'
-    search(coord_x-1, coord_y-1)
-    search(coord_x-1, coord_y)
-    search(coord_x-1, coord_y+1)
-    search(coord_x, coord_y-1)
-    search(coord_x, coord_y+1)
-    search(coord_x+1, coord_y-1)
-    search(coord_x+1, coord_y)
-    search(coord_x+1, coord_y+1)
-    print(a)
+    mode = input()
+    if mode == 'o':
+        coord_x = int(input())
+        coord_y = int(input())
+        if (coord_x, coord_y) in bomb:
+            bomb.pop(bomb.index((coord_x, coord_y)))
+        a[coord_x][coord_y] = '-'
+        search(coord_x-1, coord_y-1)
+        search(coord_x-1, coord_y)
+        search(coord_x-1, coord_y+1)
+        search(coord_x, coord_y-1)
+        search(coord_x, coord_y+1)
+        search(coord_x+1, coord_y-1)
+        search(coord_x+1, coord_y)
+        search(coord_x+1, coord_y+1)
+        print(a)
+    elif mode == 'i':
+        coord_x = int(input())
+        coord_y = int(input())
+        if (coord_x, coord_y) in bomb:
+            a[coord_x][coord_y] = '/'
+            print('ВЕРНО')
+        else:
+            print('НЕВЕРНО')
