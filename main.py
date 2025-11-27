9129b576cecb944da873493cdff4e7090ecdfe76
Saskisasff <sankinvadim545@gmail.com>
2025-11-27 21:28:59 +0300

    Добавление режима флажков

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def field_rendering(a):
         text += '\033[1;31m'+str(i)+'\033[0m'
         for j in range(9):
             text += ' '
-            if a[i][j] in ('1', '2', '3', '4', '5'):
+            if a[i][j] in ('1', '2', '3', '4', '5', '6', '/', '@'):
                 if a[i][j] == '1':
                     text += '\033[1;34m' + a[i][j] + '\033[0m'
                 elif a[i][j] == '2':
@@ -57,6 +57,12 @@ def field_rendering(a):
                     text += '\033[1;36m' + a[i][j] + '\033[0m'
                 elif a[i][j] == '5':
                     text += '\033[1;37m' + a[i][j] + '\033[0m'
+                elif a[i][j] == '6':
+                    text += '\033[1;37m' + a[i][j] + '\033[0m'
+                elif a[i][j] == '/':
+                    text += '\033[1;31m' + a[i][j] + '\033[0m'
+                elif a[i][j] == '@':
+                    text += '\033[1;30m' + a[i][j] + '\033[0m'
             else:
                 text += '\033[1;32m'+a[i][j]+'\033[0m'
         print(text)
@@ -73,27 +79,53 @@ def checking_cage(coord_x, coord_y, check):
         search(coord_x+1, coord_y+1, check)
                 
 a = np.full((9,9), '#')
+flags = 10
 bomb = []
 begin = 1
 for i in range(10):
     bomb.append((randint(0,8), randint(0,8)))
 
-for i in range(9):
-    for j in range(9):
-        if (i, j) in bomb:
-            a[i][j] = '@'
+# for i in range(9):
+#     for j in range(9):
+#         if (i, j) in bomb:
+#             a[i][j] = '@'
 print('\033[1;31m MINESWEEPER IN CONSOLE \033[0m\n\n')
 print('\033[4;37m by Saskisasff \033[0m\n')
 field_rendering(a)
 while True:
-    print('Select the mode:\no - grub\ni - to put\n')
+    print(f'Flags: {flags}')
+    if flags >0:
+        print('Select the mode:\no - grub\ni - to put\n')
+    elif flags == 0:
+        #print('Select the mode:\no - grub\n')
+        for i in range(10):
+            summ = 0
+            if a[bomb[i][0], bomb[i][1]] == '/':
+                summ += 1
+            if summ == 10:
+                print('\033[1;32m YOU WIN! \033[0m')
+                break
     mode = input()
     if mode == 'o':
         print('Enter the x and y coordinates')
         coord_x = int(input())
         coord_y = int(input())
+        if coord_x > 8 or coord_y > 8:
+            print('The value is too high!')
+            continue
+        if coord_x < -9 or coord_y < -9:
+            print('The value is too low!')
+            continue
+        if begin == 1:
+            if (coord_x, coord_y) in bomb:
+                    bomb.pop(bomb.index((coord_x, coord_y)))
+                    bomb.append((randint(0,8), randint(0,8)))
+            a[coord_x][coord_y] = ' '
+            checking_cage(coord_x, coord_y, 0)
+            field_rendering(a)
+            begin = 0
+            continue   
         if search(coord_x, coord_y, 1):
-            print('true')
             if begin == 1:
                 if (coord_x, coord_y) in bomb:
                     bomb.pop(bomb.index((coord_x, coord_y)))
@@ -107,7 +139,6 @@ while True:
             checking_cage(coord_x, coord_y, 0)
             field_rendering(a)
         else:
-            print('false')
             if begin == 1:
                 if (coord_x, coord_y) in bomb:
                     bomb.pop(bomb.index((coord_x, coord_y)))
@@ -116,6 +147,11 @@ while True:
             else:
                 if (coord_x, coord_y) in bomb:
                     print('\033[1;31m GAME OVER \033[0m\n\n')
+                    for i in range(9):
+                        for j in range(9):
+                            if (i, j) in bomb:
+                                a[i][j] = '@'
+                    field_rendering(a)         
                     break
             search(coord_x, coord_y, 0)
             field_rendering(a)
@@ -123,8 +159,12 @@ while True:
         print('Enter the x and y coordinates')
         coord_x = int(input())
         coord_y = int(input())
-        if (coord_x, coord_y) in bomb:
-            a[coord_x][coord_y] = '/'
-            print('ВЕРНО')
-        else:
-            print('НЕВЕРНО')
+        if coord_x > 8 or coord_y > 8:
+            print('The value is too high!')
+            continue
+        if coord_x < -9 or coord_y < -9:
+            print('The value is too low!')
+            continue
+        a[coord_x][coord_y] = '/'
+        flags -= 1
+        field_rendering(a)
